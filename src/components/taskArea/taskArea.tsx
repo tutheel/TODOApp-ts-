9acf9f63eb14caf539893b237233af1c6346b784
Sushil <sushilpatil953825@gmail.com>
2024-05-13 19:04:20 +0530

    Complted TaskFormsCreation Section & Added TaskCounter Section

--- src/components/taskArea/taskArea.tsx
+++ src/components/taskArea/taskArea.tsx
@@ -1,10 +1,55 @@
 import { FC, ReactElement } from 'react';
-import { Grid } from '@mui/material';
+import { Grid, Box } from '@mui/material';
+import { format } from 'date-fns';
+import { TaskCounter } from '../taskCounter/taskCounter';
+import { Task } from '../task/task';
 
 export const TaskArea: FC = (): ReactElement => {
   return (
     <Grid item md={8} px={4}>
-      <h1>Content</h1>
+      <Box
+        md={8}
+        px={4}
+        mb={4}
+        display="flex"
+        justifyContent="center"
+      >
+        <h3>
+          Status of Your Task As On{' '}
+          {format(new Date(), 'PPPP')}
+        </h3>
+      </Box>
+      <Grid
+        container
+        display="flex"
+        justifyContent="center"
+      >
+        <Grid
+          item
+          display="flex"
+          flexDirection="row"
+          justifyContent="space-around"
+          alignItems="center"
+          md={10}
+          xs={12}
+          mb={8}
+        >
+          <TaskCounter />
+          <TaskCounter />
+          <TaskCounter />
+        </Grid>
+        <Grid
+          item
+          display="flex"
+          flexDirection="column"
+          xs={10}
+          md={8}
+        >
+          <Task />
+          <Task />
+          <Task />
+        </Grid>
+      </Grid>
     </Grid>
   );
 };
---
+++ src/components/taskCounter/taskCounter.tsx
@@ -0,0 +1,58 @@
+import { FC, ReactElement } from 'react';
+import { Typography, Box, Avatar } from '@mui/material';
+import { ITaskCounter } from './interfaces/ITaskCounter';
+import { Status } from '../createTaskForms/enums/Status';
+import { emitCorrectBorderColor } from './helpers/emitCorrectBorderColor';
+import { emitCorrectLabel } from './helpers/emitCorrectLabel';
+import PropTypes from 'prop-types';
+
+export const TaskCounter: FC<ITaskCounter> = (
+  props
+): ReactElement => {
+  const { status = Status.completed, count = 0 } = props;
+
+  return (
+    <>
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Avatar
+          sx={{
+            backgroundColor: 'transparent',
+            border: '5px solid',
+            width: '96px',
+            height: '96px',
+            marginBottom: '16px',
+            borderColor: `${emitCorrectBorderColor(
+              status
+            )}`,
+          }}
+        >
+          <Typography color="#ffffff" variant="h4">
+            {count}
+          </Typography>
+        </Avatar>
+        <Typography
+          color="#ffffff"
+          fontWeight="bold"
+          fontSize="20px"
+          variant="h5"
+        >
+          {emitCorrectLabel(status)}
+        </Typography>
+      </Box>
+    </>
+  );
+};
+
+TaskCounter.propTypes = {
+  count: PropTypes.number,
+  status: PropTypes.oneOf([
+    Status.todo,
+    Status.inProgress,
+    Status.completed,
+  ]),
+};
