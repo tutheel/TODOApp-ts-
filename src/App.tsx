82ae0af61094b70ebcb9905ffdd23cf0418a3673
Sushil <sushilpatil953825@gmail.com>
2024-05-21 23:51:22 +0530

    Added Tanstack-Query in App.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import React, { FC, ReactElement } from 'react';
 import { ThemeProvider, CssBaseline } from '@mui/material';
-import { customTheme } from './theme/customeTheme'; // corrected import path
+import { customTheme } from './theme/customeTheme';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
 import { Dashboard } from './pages/dashboard/dashboard';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
 
 const App: FC = (): ReactElement => {
   return (
-    <ThemeProvider theme={customTheme}>
-      <CssBaseline />
-      <Dashboard></Dashboard>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={customTheme}>
+        <CssBaseline />
+        <Dashboard></Dashboard>
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 
